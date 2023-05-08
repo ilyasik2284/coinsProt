59481a72a0eea7d373c542d365d8cd067091da3f
ilyasik2284 <p.ilya228335@gmail.com>
2023-05-08 16:05:00 +0300

    остыл, пытаюсь исправить проект

--- RealGame/Assets/Resources/Scripts/PlayerController.cs
+++ RealGame/Assets/Resources/Scripts/PlayerController.cs
@@ -3,7 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CameraFollow : MonoBehaviour
+public class PlayerController : MonoBehaviour
 {
     public Transform target;
     [SerializeField] private float bounderX = 0.15f;
