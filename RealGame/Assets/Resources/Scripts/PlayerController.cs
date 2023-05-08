4ef01a028669eea2bde3861ec450bd3de796a605
ilyasik2284 <p.ilya228335@gmail.com>
2023-05-08 17:13:00 +0300

    построил карту, восстановил скрипты

--- RealGame/Assets/Resources/Scripts/PlayerController.cs
+++ RealGame/Assets/Resources/Scripts/PlayerController.cs
@@ -1,50 +1,49 @@
-
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class PlayerController : MonoBehaviour
 {
-    public Transform target;
-    [SerializeField] private float bounderX = 0.15f;
-    [SerializeField] private float bounderY = 0.05f;
+    private RaycastHit2D hit;
+    private Vector3 moveSide;
+    private BoxCollider2D boxCollider2d;
 
-
-    private void Awake()
+    void Awake()
     {
-        target = GetComponent<Transform>();
-    }
-    void LateUpdate()
-    {
-        Vector3 delta = Vector3.zero;
-        float deltaX = target.position.x - transform.position.x;
-
-        if (deltaX > bounderX || deltaX < -bounderX)
-        {
-            if (transform.position.x < target.position.x)
-            {
-                delta.x = deltaX - bounderX;
-            }
-            else
-            {
-                delta.x = deltaX + bounderX;
-            }
-        }
-
-        float deltaY = target.position.y - transform.position.y;
-
-        if (deltaY > bounderY || deltaY < -bounderY)
-        {
-            if (transform.position.y < target.position.y)
-            {
-                delta.y = deltaY - bounderY;
-            }
-            else
-            {
-                delta.y = deltaY + bounderY;
-            }
-        }
-        transform.position += new Vector3(delta.x, delta.y, 0);
+        boxCollider2d = GetComponent<BoxCollider2D>();
     }
 
+    void FixedUpdate()
+    {
+        float x = Input.GetAxisRaw("Horizontal");
+        float y = Input.GetAxisRaw("Vertical");
+
+        moveSide = new Vector3(x, y, 0);
+
+        if (moveSide.x > 0)
+        {
+            transform.localScale = Vector3.one;
+        }
+        else if (moveSide.x < 0)
+        {
+            transform.localScale = new Vector3(-1, 1, 1);
+
+        }
+           
+
+
+        hit = Physics2D.BoxCast(transform.position, boxCollider2d.size, 0, new Vector2(moveSide.x, 0), Mathf.Abs(moveSide.x * Time.deltaTime), LayerMask.GetMask("Player", "Walls"));
+        if (hit.collider == null)
+        {
+            transform.Translate(moveSide.x * Time.deltaTime, 0, 0);
+        }
+
+
+        hit = Physics2D.BoxCast(transform.position, boxCollider2d.size, 0, new Vector2(0, moveSide.y), Mathf.Abs(moveSide.y * Time.deltaTime), LayerMask.GetMask("Player", "Walls"));
+        if (hit.collider == null)
+        {
+            transform.Translate(0, moveSide.y * Time.deltaTime, 0);
+        }
+    }
 }
